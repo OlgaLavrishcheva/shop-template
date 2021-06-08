c338d95561a7d5d4d087dcd25978d490c20627d0
OlgaLavrishcheva <76204278+OlgaLavrishcheva@users.noreply.github.com>
2021-06-08 21:41:55 +0300

    added a header css .page-header__cart

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,4 @@
+// Пошаговый план реализации интерактивности:
 // 1. Добавляем обработчики на кнопки "Add to card", реализуем делегирование
 // 2. Реализуем счетчик добавленных в корзину товаров.
 // 3. Реализуем двухсекундную блокировку свех кнопок между нажатиями.
@@ -7,14 +8,12 @@
 // 6. Реализуем счет всех товаров, добавленных в корзину,
 //    учесть ошибку JS при рабте с дробной частью
 
-// 1. Добавляем обработчики на кнопки "Add to card", реализуем делегирование
-
 const buttonsContainer = document.querySelector('#content-container');
 
 const btnClickHandler = (e) => {
     const target = e.target;
 
-    if (target && target.matches('button.item-actions__cart')) console.log('clicked');
+    if (target && target.matches('.item-actions__cart')) console.log('clicked');
 };
 
 buttonsContainer.addEventListener('click', btnClickHandler);
